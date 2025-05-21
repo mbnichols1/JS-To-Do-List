d0d763d82585d97f1d245b0c8315bf0d43ca4f84
mbnichols1 <164078337+mbnichols1@users.noreply.github.com>
2025-05-20 22:35:32 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,54 @@
+let todos = JSON.parse(localStorage.getItem("todos")) || [];
+
+function saveTodos() {
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function renderTodos() {
+  const list= document.getElementById("todo-list");
+  list.innerHTML = "";
+
+  todos.forEach((todo, index) => {
+    const li = document.createElement("li");
+    li.className = todo.done ? "completed" : "";
+
+    li.textContent = todo.text;
+    li.onclick = () => toggleTodo(index);
+
+    const del = document.createElement("button");
+    del.textContent = "X";
+    del.onClick = (e) => {
+      e.stopPropagation();
+      deleteTodo(index);
+    };
+
+    li.appendChild(del);
+    list.appendChild(li);
+  });
+}
+
+function addTodo() {
+  const input = docuent.getElementById("todo-input");
+  const text = input.value.trim();
+  if (text) {
+    todos.push({ text, done: false });
+    saveTodos();
+    renderTodos();
+    input.value = "";
+  }
+}
+
+function toggleTodo(index) {
+  todos[index].done = !todos[index].done;
+  saveTodos();
+  renderTodos();
+}
+
+function deleteTodo(index) {
+  todos.splice(index, 1);
+  saveTodos();
+  renderTodos();
+}
+
+renderTodos();
+    
